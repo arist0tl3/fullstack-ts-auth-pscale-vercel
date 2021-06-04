3cae6100022ad5d3e814228b85d533f610d5e50c
Sean <sean@vigorwebsolutions.com>
2021-06-04 08:57:37 -0700

    Remove currentUser check (testing)

--- server/src/data/User/Query/users.ts
+++ server/src/data/User/Query/users.ts
@@ -2,10 +2,6 @@ import { User } from 'generated/graphql';
 import { Context } from 'types/context';
 
 const users = async (root: object, args: any, ctx: Context): Promise<User[]> => {
-  const { currentUser } = ctx;
-
-  if (!currentUser) return [];
-
   const query = `
   SELECT
     id, email, password
